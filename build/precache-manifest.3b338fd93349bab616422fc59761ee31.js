69576e764217aa079d2ede8bce4573a094d169dd
Vishakh Dadwal <vishakhdadwal999@gmail.com>
2019-12-08 15:29:32 +0530

    initial  commit

---
+++ build/precache-manifest.3b338fd93349bab616422fc59761ee31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef64cb44f3c465166ca8d57a98b7297",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7710a4641dc49de81865",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b1a936948dcd9f8da817",
+    "url": "/static/js/2.59d6e834.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.59d6e834.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7710a4641dc49de81865",
+    "url": "/static/js/main.67806d44.chunk.js"
+  },
+  {
+    "revision": "61dbae8247f5592690a1",
+    "url": "/static/js/runtime-main.be0986ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
